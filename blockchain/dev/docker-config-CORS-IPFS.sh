6d54396101e5ccb19e201443122eaafd04ff1f9b
tiennt0212 <tiennt0212@gmail.com>
2022-03-12 13:08:13 +0700

    :zap: shell script to config CORS for ipfs

---
+++ blockchain/dev/docker-config-CORS-IPFS.sh
@@ -0,0 +1,5 @@
+docker exec ipfs0 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
+docker exec ipfs0 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["GET", "POST"]'
+docker exec ipfs0 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Headers '["Authorization"]'
+docker exec ipfs0 ipfs config --json API.HTTPHeaders.Access-Control-Expose-Headers '["Location"]'
+docker exec ipfs0 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Credentials '["true"]'
